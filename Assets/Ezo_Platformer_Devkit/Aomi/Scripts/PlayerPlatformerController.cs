b89400ecd6fc98a6ae23646db945b93cb397a866
Unknown <shobu13@hotmail.fr>
2018-07-30 02:04:41 +0200

    first commit

---
+++ Assets/Ezo_Platformer_Devkit/Aomi/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+    public float MaxSpeed = 7;
+    public float JumpTakeOffSpeed = 7;
+    public int PointPerItem = 10;
+    public Text ScoreText;
+    public Tilemap ColliderTilemap;
+    public Tilemap CheckpointTilemap;
+
+    public Text DebugPos, DebugColliderPos;
+    public Tilemap Debug;
+
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private int _playerPoint = 0;
+    private Vector2 _spawnPoint;
+
+    // Use this for initialization
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        _spawnPoint = GetComponent<Transform>().position;
+        ColliderTilemap.GetComponent<TilemapRenderer>().enabled = false;
+        CheckpointTilemap.GetComponent<TilemapRenderer>().enabled = false;
+    }
+
+
+    protected override void ComputeVelocity()
+    {
+        DebugPos.text = GetComponent<Transform>().position.ToString();
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = JumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if (move.x > 0.01f)
+        {
+            if (_spriteRenderer.flipX == true)
+            {
+                _spriteRenderer.flipX = false;
+            }
+        }
+        else if (move.x < -0.01f)
+        {
+            if (_spriteRenderer.flipX == false)
+            {
+                _spriteRenderer.flipX = true;
+            }
+        }
+
+        _animator.SetBool("grounded", grounded);
+        _animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / MaxSpeed);
+
+        targetVelocity = move * MaxSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("objet"))
+        {
+            other.GetComponent<ParticleSystem>().Play();
+            _playerPoint += PointPerItem;
+            UpdateScoreText();
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("collider"))
+        {
+            GetComponent<Transform>().position = _spawnPoint;
+        }
+
+        if (other.gameObject.CompareTag("checkpoint"))
+        {
+            Tilemap tilemap = other.GetComponent<Tilemap>();
+            Vector3Int tilePosition = tilemap.WorldToCell(GetComponent<Transform>().position);
+            Vector3Int[] tabTile = new Vector3Int[9];
+            tabTile[0] = tilePosition;
+            tabTile[1] = tilePosition + new Vector3Int(1, 0, 0);
+            tabTile[2] = tilePosition + new Vector3Int(-1, 0, 0);
+            tabTile[3] = tilePosition + new Vector3Int(0, 1, 0);
+            tabTile[4] = tilePosition + new Vector3Int(0, -1, 0);
+            tabTile[5] = tilePosition + new Vector3Int(-1, 1, 0);
+            tabTile[6] = tilePosition + new Vector3Int(1, -1, 0);
+            tabTile[7] = tilePosition + new Vector3Int(1, 1, 0);
+            tabTile[8] = tilePosition + new Vector3Int(-1, -1, 0);
+            foreach (Vector3Int tilePos in tabTile)
+            {
+                if (tilemap.HasTile(tilePos))
+                {
+                    UnityEngine.Debug.Log("tilePos");
+                    UnityEngine.Debug.Log(tilePos);
+                    tilePosition = tilePos;
+                    UnityEngine.Debug.Log("tilemap.GetCellCenterWorld(tilePosition)");
+                    UnityEngine.Debug.Log(tilemap.GetCellCenterWorld(tilePosition));
+                    Vector3 tileWorldPosition = tilemap.GetCellCenterWorld(tilePosition) + new Vector3(0, 0.6f, 0);
+                    _spawnPoint = tileWorldPosition;
+                    DebugColliderPos.text = _spawnPoint.ToString();
+                }
+            }
+        }
+    }
+
+    void UpdateScoreText()
+    {
+        ScoreText.text = "Score : " + _playerPoint.ToString();
+    }
+}
---
+++ Assets/Ezo_Platformer_Devkit/Scripts/scrollingBackground.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingBackground : MonoBehaviour {
+
+    public float backgroundSize;
+    public float paralaxSpeed;
+
+    private Transform _cameraTransform;
+    private Transform[] _layers;
+    private float _viewZone = 10;
+    private int _leftIndex;
+    private int _rightIndex;
+    private float _lastCameraX;
+
+    private void Start()
+    {
+        _cameraTransform = Camera.main.transform;
+        _lastCameraX = _cameraTransform.position.x;
+        _layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _layers[i] = transform.GetChild(i);
+        }
+
+        _leftIndex = 0;
+        _rightIndex = _layers.Length - 1;
+    }
+
+    private void Update()
+    {
+        float deltaX = _cameraTransform.position.x - _lastCameraX;
+        transform.position += new Vector3(deltaX * paralaxSpeed, 0, 0);
+        _lastCameraX = _cameraTransform.position.x;
+
+        if (_cameraTransform.position.x < (_layers[_leftIndex].transform.position.x + _viewZone))
+            ScrollLeft();
+
+        if (_cameraTransform.position.x > (_layers[_rightIndex].transform.position.x - _viewZone))
+            ScrollRight();
+    }
+
+    private void ScrollLeft()
+    {
+        int lastRight = _rightIndex;
+        _layers[lastRight].position = new Vector3((_layers[_leftIndex].position.x - backgroundSize), _layers[_leftIndex].position.y, 0);
+        _leftIndex = _rightIndex;
+        _rightIndex--;
+        if (_rightIndex < 0)
+            _rightIndex = _layers.Length - 1;
+    }
+
+    private void ScrollRight()
+    {
+        int lastLeft = _leftIndex;
+        _layers[lastLeft].position = new Vector3((_layers[_rightIndex].position.x + backgroundSize), _layers[_rightIndex].position.y, 0);
+        _rightIndex = _leftIndex;
+        _leftIndex++;
+        if (_leftIndex >= _layers.Length)
+            _leftIndex = 0;
+    }
+}
