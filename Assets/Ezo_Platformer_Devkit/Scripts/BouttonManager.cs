0ddf47171bce3c820db549c04f0b6d28b64ed20a
Unknown <shobu13@hotmail.fr>
2018-07-30 22:42:17 +0200

    30/07/2018 22:41
    ajout menu et scénar

---
+++ Assets/Ezo_Platformer_Devkit/Scripts/BouttonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BouttonManager : MonoBehaviour {
+
+	public void Jouer()
+    {
+        SceneManager.LoadScene("ProtoLevel1");
+    }
+
+    public void Option()
+    {
+        SceneManager.LoadScene("Option");
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
