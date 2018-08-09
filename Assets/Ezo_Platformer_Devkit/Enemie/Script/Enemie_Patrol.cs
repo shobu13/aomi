74e04e72090ee53e0cb7553ad1369ec43f5616d5
Damo <dcles123@gmail.com>
2018-08-09 14:21:32 +0200

    IA FONCTIONNEL 09/08 14:21

--- Assets/Ezo_Platformer_Devkit/Enemie/Script/Enemie_Patrol.cs
+++ Assets/Ezo_Platformer_Devkit/Enemie/Script/Enemie_Patrol.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Enemie_Patrol : MonoBehaviour {
 
-
     public float Distance;
-    private RaycastHit2D hit;
+    RaycastHit2D[] hit;
+    public GameObject Cible;
 
-    void Update () {
+     void Start()
+     {
 
-        hit = Physics2D.Raycast(gameObject.transform.position, gameObject.transform.right);
-        Debug.DrawRay(gameObject.transform.position, hit.point, Color.red, 3f);
+        Cible = GameObject.Find("Aomi_CameraTest");
 
-	}
+     }
 
+    void Update() {
+
+        hit = Physics2D.LinecastAll(gameObject.transform.position, Cible.transform.position);
+        for (int i = 0; i < hit.Length; i++) {
+            Debug.DrawLine(gameObject.transform.position, hit[i].point, Color.red, 2f);
+            if (hit[i].collider)
+            {
+
+                Debug.Log(hit[i].collider.name);
+                if (hit[i].collider.CompareTag("Player") && hit[i].distance < Distance)
+                {
+
+                    gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, hit[i].transform.position, 0.025f);
+                }
+            }
+        }
+    }
 }
