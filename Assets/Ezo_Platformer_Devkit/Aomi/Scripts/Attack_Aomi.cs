a64fb126b5b7d05a6dcd0a135ecedd5c15ff13ad
Damo <dcles123@gmail.com>
2018-08-14 14:18:13 +0200

    ATTACK CHECK

---
+++ Assets/Ezo_Platformer_Devkit/Aomi/Scripts/Attack_Aomi.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Aomi : MonoBehaviour {
+
+    public Animator animator;
+
+    bool Attack;
+
+	void Start () {
+
+        Attack = false;
+        animator = gameObject.GetComponent<Animator>();
+
+	}
+	
+	void Update () {
+
+
+        if (Input.GetKeyDown("x") && !Attack)
+        {
+
+            Attack = true;
+            animator.SetBool("Attack1", true);
+            StartCoroutine("Wait");
+
+        }
+
+	}
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Attack = false;
+        animator.SetBool("Attack1", false);
+    }
+
+}
